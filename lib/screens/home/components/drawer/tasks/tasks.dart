154a830e6fbe6b76604d64a87f239ce148311c91
Am1r__8 <68626539+CoachAlpha@users.noreply.github.com>
2022-11-27 16:49:29 -0500

    adding home components (tasks)

--- lib/screens/home/components/drawer/tasks/tasks.dart
+++ lib/screens/home/components/drawer/tasks/tasks.dart
@@ -1,88 +1,67 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:get/get.dart';
-import 'package:circular_check_box/circular_check_box.dart';
-import 'package:google_fonts/google_fonts.dart';
-import 'package:ms_undraw/ms_undraw.dart';
 import 'package:todo/constants/colors.dart';
-import 'package:todo/controllers/main-controller.dart';
+import 'package:todo/models/tasks.dart';
 import 'package:todo/screens/home/animations.dart';
-import 'package:todo/screens/home/components/tasks/item.dart';
+import 'package:todo/screens/home/components/tasks/items.dart';
 
-class HomeTasksItems extends StatefulWidget {
-  @override
-  _HomeTasksItemsState createState() => _HomeTasksItemsState();
-}
-
-class _HomeTasksItemsState extends State<HomeTasksItems> {
+class HomeTasks extends StatelessWidget {
+  Duration _animationDuration = Duration(milliseconds: 500);
   bool _isDark;
   @override
   Widget build(BuildContext context) {
     final Brightness brightnessValue =
         MediaQuery.of(context).platformBrightness;
     _isDark = brightnessValue == Brightness.dark;
-    return GetBuilder<HomeAnimationsController>(builder: (__) {
-      return GetBuilder<MainController>(
-        builder: (_) {
-          return AnimatedSwitcher(
-            duration: Duration(milliseconds: 0),
-            child: _.tasks.length != 0
-                ? ListView.builder(
-                    reverse: true,
-                    shrinkWrap: true,
-                    physics: BouncingScrollPhysics(),
-                    itemCount: _.tasks.length,
-                    itemBuilder: (context, index) {
-                      return TasksItem(
-                        index: index,
-                      );
-                    },
-                  )
-                : AnimatedOpacity(
-                    opacity: __.notFoundOpacity,
-                    duration: Duration(milliseconds: 500),
-                    child: Container(
-                        child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        SizedBox(
-                          height: 100,
-                        ),
-                        Text(
-                          "You have no task for today!",
-                          style: GoogleFonts.ubuntu(
-                              color: _isDark
-                                  ? kBackgroundColor.withOpacity(0.8)
-                                  : kSecondaryColor,
-                              fontSize: 22.2,
-                              fontWeight: FontWeight.w700),
-                        ),
-                        Padding(
-                          padding: EdgeInsets.only(left: 50, right: 50),
-                          child: UnDraw(
-                            height: MediaQuery.of(context).size.width - 100,
-                            color: _isDark
-                                ? kBackgroundColor.withOpacity(0.8)
-                                : kSecondaryColor,
-                            illustration: UnDrawIllustration.not_found,
-                            placeholder: Padding(
-                              padding: EdgeInsets.only(top: 100),
-                              child: SpinKitDoubleBounce(
-                                color: _isDark
-                                    ? kBackgroundColor.withOpacity(0.8)
-                                    : kSecondaryColor,
-                                size: 75,
-                              ),
-                            ),
-                            errorWidget: Icon(Icons.error_outline,
-                                color: Colors.red, size: 50),
-                          ),
-                        ),
-                      ],
-                    )),
+    return GetBuilder<HomeAnimationsController>(builder: (_) {
+      return Padding(
+        padding: EdgeInsets.only(top: 50.0),
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(left: 25.0),
+                  child: AnimatedOpacity(
+                    duration: _animationDuration,
+                    opacity: _.tasksTitleOpacity,
+                    child: Text(
+                      "TODAY'S TASKS",
+                      style: TextStyle(
+                          fontSize: 12.5,
+                          fontWeight: FontWeight.bold,
+                          color: _isDark
+                              ? kBackgroundColor.withOpacity(0.5)
+                              : Colors.black.withOpacity(0.3)),
+                    ),
                   ),
-          );
-        },
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(right: 25.0),
+                  child: AnimatedOpacity(
+                      duration: _animationDuration,
+                      opacity: _.tasksTitleOpacity,
+                      child: IconButton(
+                          icon: Icon(FontAwesomeIcons.checkDouble,
+                              size: 22.5,
+                              color: _isDark
+                                  ? kBackgroundColor.withOpacity(0.5)
+                                  : Colors.black.withOpacity(0.3)),
+                          onPressed: () async {
+                            await Future.delayed(Duration(milliseconds: 111));
+                            doneAllTasks();
+                          })),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            HomeTasksItems()
+          ],
+        ),
       );
     });
   }
